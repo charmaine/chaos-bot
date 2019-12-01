4772996bed0c026655f8f1fd948d78790a2a13b3
Charmaine Lee <me@charmaineklee.com>
2019-11-30 17:55:46 -0800

    complete

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
 require('dotenv').config();
-//
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const WebClient = require('@slack/client').WebClient;
-const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
-var request = require("request");
-
-// Retrieve bot token from dotenv file
-const bot_token = process.env.SLACK_BOT_TOKEN || '';
-
-// Authorization token
-const auth_token = process.env.SLACK_AUTH_TOKEN || '';
-
-// Verification token for Events Adapter, to make sure events we receive are from Slack
-const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
-
-// Slack web client
-const web = new WebClient(auth_token);
-const bot = new WebClient(bot_token);
+const request = require("request");
 
 // Creates express app
 const app = express();
+// The port used for Express server
+const PORT = 4390;
+// Starts server
+app.listen(process.env.PORT || PORT, function() {
+  console.log('Bot is listening on port ' + PORT);
+});
 
-// Use BodyParser for app
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// The port used for  Express server
-const PORT = 4390;
-
-let channel = '#general'
-let botID;
-
-// Starts server
-// Starts our server
-// Heroku dynamically assigns your app a port so must do OR statement
-app.listen(process.env.PORT || PORT, function() {
-  console.log('Bot is listening on port ' + PORT);
-
-  // Get userID for bot
-  bot.auth.test()
-		.then((info) => {
-			if (info.user_id) {
-				botID = info.user_id;
-			}
-		})
-		.catch(console.error)
-});
-
-
 app.post('/', (req, res) => {
-var data = {form: {
-      token: process.env.SLACK_AUTH_TOKEN,
-      channel: "#general",
-      text: "Hi! :wave: \n I'm your new bot."
-    }};
-request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
-      // Sends welcome message
-      res.json();
+  var data = {form: {
+    token: process.env.SLACK_AUTH_TOKEN,
+    channel: "#general",
+    text: res.req.body.text,
+  }};
+
+  var originalChannel = res.req.body.channel_id;
+  console.log(originalChannel);
+
+  var originalUser = res.req.body.user_id;
+
+  request.get('https://slack.com/api/conversations.list?token=' + process.env.SLACK_AUTH_TOKEN, function (req, res){
+
+    let channels = JSON.parse(res.body).channels;
+    let randomChannelNum = Math.floor(Math.random()*channels.length);
+    let randomChannel = channels[randomChannelNum];
+    data.form.channel = randomChannel.id;
+
+    request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+      // console.log(res.channel);
     });
+
+    // console.log(res);
+
+    let funnyArray = [" rewired ", " beep booped ", " unfortunately headed out ", " yeeted "]
+    var data2 = {form: {
+      token: process.env.SLACK_AUTH_TOKEN,
+      channel: originalChannel,
+      text: "yPur mesAGe haas been" + funnyArray[Math.floor(Math.random()*funnyArray.length)]+ "to: #" + randomChannel.name,
+      user: originalUser
+    }};
+
+    if (randomChannel.id == originalChannel) {
+      data2.form.text = "luKC yyou! your msg made it bk to the same ChanNELllll wohoo , try again"
+    }
+
+    request.post('https://slack.com/api/chat.postEphemeral', data2, function (error, response, body) {
+      // console.log(res.channel);
+    });
+  });
+  res.end();
 });
